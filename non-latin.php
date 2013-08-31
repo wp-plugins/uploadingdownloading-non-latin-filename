f3e7385d17af429d831df88bdb7fe4dbc15b8697
mytory <mytory@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-31 11:00:18 +0000

    tag 1.1
    git-svn-id: https://plugins.svn.wordpress.org/uploadingdownloading-non-latin-filename/tags/1.1@765077 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- non-latin.php
+++ non-latin.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Plugin Name: uploading downloading non-latin filename
-Description: WordPress cannot attach files with non-latin file name (e.g., Korean). This is the one major drawback to popularization of WordPress among non-english users. This plugin will rename the file (with latin or non-latin names) to numbers, stores the original file name as a title of media post, and upload the file to the server. When a user attempts to download the file, the file will be returned with corresponding media post's title. But image files will not be processed as such: image files will be returned with numbered name. Because, src value of shoud be real filename on server. 
+Description: You can upload files has non-latin filename and download original filename. 이 플러그인을 사용하면 파일명이 한글인 파일을 올릴 수 있고, 다운받을 때는 원래 파일명으로 다운받을 수 있다.
 Author: Ahn, Hyoung-woo
 Author URI: http://mytory.co.kr
-Version: 1.0.9
+Version: 1.1
 License: GPL2 (http://www.gnu.org/licenses/gpl-2.0.html)
 */
 
@@ -16,15 +16,27 @@ License: GPL2 (http://www.gnu.org/licenses/gpl-2.0.html)
  * @param array $file
  */
 function nlf_prefilter($file) {
+	global $current_user;
+	$unique_id = "{$_SERVER['REQUEST_TIME']}-$current_user->ID";
+
+	// 파일명에 비알파벳이 없다면 파일명 변경은 필요없다.
+	if( $file['name'] == nlf_remove_nonlatin_char($file['name']) ){
+		update_user_meta( $current_user->ID, "nlf-need-change-{$unique_id}", "no");
+		return $file;
+	}
+
+	update_user_meta( $current_user->ID, "nlf-need-change-{$unique_id}", "yes");
+
+	$unique_id = "{$_SERVER['REQUEST_TIME']}-$current_user->ID";
 	$path_info = pathinfo($file['name']);
-	
-	$_SESSION['original_filename'] = $file['name'];
+	update_user_meta($current_user->ID, "nlf-original-filename-{$unique_id}", $file['name']);
 	$new_filename = date('Ymd_His');
-	$_SESSION['new_filename'] = $new_filename;
+	update_user_meta($current_user->ID, "nlf-new-filename-{$unique_id}", $new_filename);
 	$file['name'] = $new_filename . '.' . $path_info['extension']; 
 	
 	return $file;
 }
+add_filter('wp_handle_upload_prefilter', 'nlf_prefilter');
 
 /**
  * set post_title by original filename.
@@ -33,61 +45,66 @@ function nlf_prefilter($file) {
  * @param int $attachment
  */
 function nlf_add_attachment($attachment){
+	global $current_user;
+
+	$unique_id = "{$_SERVER['REQUEST_TIME']}-$current_user->ID";
 	$post = get_post($attachment);
-	//포스트 타이틀이 없다면 원 파일명으로 첨부파일의 타이틀을 넣어 준다.
-	if( $_SESSION['new_filename'] == $post->post_title ){
-		$post->post_title = $_SESSION['original_filename'];
-		//첨부파일 타이틀 업데이트에 실패하면 에러 메시지를 출력하고 죽는다.
-		if( wp_update_post((array)$post) === 0 ){
-			echo 'error occured! - ';
-			if($current_user->allcaps['level_10']){
-				echo __FILE__ . ' LINE  ' . __LINE__;
-				echo '<br>파일명과 줄 번호는 관리자에게만 보입니다.';
+
+	$nlf_need_change = get_user_meta($current_user->ID, "nlf-need-change-{$unique_id}", true);
+
+	// 파일명 변경이 필요한 경우
+	if($nlf_need_change == 'yes'){
+		$new_filename = get_user_meta($current_user->ID, "nlf-new-filename-{$unique_id}", true);
+		$orginal_filename = get_user_meta($current_user->ID, "nlf-original-filename-{$unique_id}", true);
+
+		//포스트 타이틀을 사용자가 따로 넣어 주지 않았다면 원 파일명으로 첨부파일의 타이틀을 넣어 준다.
+		if( $new_filename == $post->post_title ){
+			$post->post_title = $orginal_filename;
+
+			//첨부파일 타이틀 업데이트에 실패하면 에러 메시지를 출력하고 죽는다.
+			if( wp_update_post((array)$post) === 0 ){
+				echo 'error occured!';
+				if(current_user_can('edit_files')){
+					echo '- ' . __FILE__ . ' LINE  ' . __LINE__;
+					echo '(파일명과 줄 번호는 파일 편집 권한이 있는 사람에게만 보입니다. 따로 권한을 변경하지 않았다면 파일 편집 권한은 관리자에게만 있습니다.)';
+				}
+				exit;
 			}
-			exit;
 		}
+		
+		// 다 사용한 메타 정보는 삭제한다.
+		delete_user_meta($current_user->ID, "nlf-new-filename-{$unique_id}");
+		delete_user_meta($current_user->ID, "nlf-original-filename-{$unique_id}");
+		delete_user_meta($current_user->ID, "nlf-need-change-{$unique_id}");
 	}
-	unset($_SESSION['original_filename']);
-	unset($_SESSION['new_filename']);
 }
-
-/**
- * Set file download url from download.php script url that plugin has.
- * 파일 다운로드 url을 파일의 url을 직접 거는 대신에 플러그인이 만든 download 스크립트의 url로 바꿔 준다. 
- * 그렇게 함으로써 다운받을 때 첨부파일 포스트의 타이틀로 파일명을 대체해 준다.
- * @param string $url
- * @param int $post_id
- */
-function nlf_wp_get_attachment_url($url, $post_id){
-	//이미지면 실제 url을 돌려 준다.
-	if( wp_attachment_is_image($post_id)){
-		return $url;
-	}
-	
-	//EG_Attchments 플러그인과 충돌하지 않도록 함.
-	$backgrace = debug_backtrace();
-	foreach ($backgrace as $arr) {
-		if(isset($arr['class']) && $arr['class'] == 'EG_Attachments'){
-			return $url;
-		}
-	}
-	
-	$url = site_url('wp-content/plugins/uploadingdownloading-non-latin-filename/download.php?id=' . $post_id);
-	return $url;
-}
-
-add_filter('wp_handle_upload_prefilter', 'nlf_prefilter');
 add_action('add_attachment', 'nlf_add_attachment');
-add_filter('wp_get_attachment_url', 'nlf_wp_get_attachment_url', '', 2);
+
+// TODO 첨부파일의 ID도 집어 넣지 않는다.
+// TODO 다운로드 링크는 js ajax로 처리한다.
+// 기존에 올린 첨부파일의 경로를 수정하는 기능은 차차 만든다.
 
 /**
  * ========================= for GD bbPress Attachment =================================
  */
 
 function nlf_enqueue_script() {
-	wp_enqueue_script('nlf_gd_bbpress_attachment', plugins_url('uploadingdownloading-non-latin-filename/non-latin.js'), array('jquery'), '1.0.9', 1);
-	wp_localize_script( 'nlf_gd_bbpress_attachment', 'nlf', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('nlf-common', plugin_dir_url(__FILE__) . 'non-latin.js', array('jquery'), '1.1', TRUE);
+
+	$plugin_url = plugin_dir_url(__FILE__);
+	$wp_upload_dir = wp_upload_dir();
+	$upload_baseurl = $wp_upload_dir['baseurl'];
+
+	$nlf_arr = array( 
+		'ajaxurl' => admin_url( 'admin-ajax.php' ),
+		'plugin_url' => $plugin_url,
+		'upload_baseurl' => $upload_baseurl . '/',
+	);
+
+	wp_localize_script( 'nlf-common', 'nlf', $nlf_arr);
 }
+add_action('wp_enqueue_scripts', 'nlf_enqueue_script');
 
 /**
  * Get filename from attachment title.
@@ -100,10 +117,11 @@ function nlf_get_filename_for_download($attchment_id){
 	$extension = pathinfo($file, PATHINFO_EXTENSION);
 	$post_title_extenstion = pathinfo($attachment->post_title, PATHINFO_EXTENSION);
 	if($extension != $post_title_extenstion){
-		$filename_for_download = sanitize_file_name($attachment->post_title) . '.' . $extension;
+		$filename_for_download = nlf_sanitize_file_name( $attachment->post_title ) . '.' . $extension;
 	}else{
-		$filename_for_download = sanitize_file_name($attachment->post_title);
+		$filename_for_download = nlf_sanitize_file_name( $attachment->post_title );
 	}
+
 	return $filename_for_download;
 }
 
@@ -115,10 +133,93 @@ function nlf_print_filename_for_download(){
 	echo nlf_get_filename_for_download($_GET['id']);
 	die();
 }
-
 add_action("wp_ajax_filename_for_download", "nlf_print_filename_for_download");
 add_action("wp_ajax_nopriv_filename_for_download", "nlf_print_filename_for_download");
-add_action('wp_enqueue_scripts', 'nlf_enqueue_script');
 
+/**
+ * ajax로 첨부파일 URL 배열을 받아서 nlf download.php?id=000 형태의 url객체 배열을 json형태로 만들어서 출력
+ */
+function nlf_get_download_url(){
+	global $wpdb;
+	$result = array();
+	foreach ($_REQUEST['attachments'] as $guid) {
+		$post = $wpdb->get_row("SELECT * FROM $wpdb->posts WHERE guid = '{$guid}' AND post_type = 'attachment'");
+		if($post){
+			$result[] = array(
+				'guid' => $guid,
+				'download_url' => plugin_dir_url(__FILE__) . 'download.php?id=' . $post->ID
+			);	
+		}
+	}
+	echo json_encode($result);
+	die();
+}
+add_action("wp_ajax_nlf_get_download_url", "nlf_get_download_url");
+add_action("wp_ajax_nopriv_nlf_get_download_url", "nlf_get_download_url");
+
+/**
+ * 플러그인을 적용할지 판단할 때 쓰는 파일명 검사 함수.
+ * 알파벳은 일단 소문자로 변경해 버린다.
+ * 그리고 나서 파일명의 알파벳, 숫자, _, -, 공백만 남기고 나머지는 모두 제거한다.
+ * @param string $filename The filename to be sanitized
+ * @return string The sanitized filename
+ */
+function nlf_remove_nonlatin_char( $filename ){
+	$raw_filename = $filename;
+	$filename = strtolower( $filename );
+	$filename = preg_replace( '/[^a-z0-9_\-\. ]/', '', $filename );
+	return trim($filename);
+}
+
+/**
+ * wp의 sanitize_file_name과 두 가지만 빼고 똑같다.
+ * 1. '\s' 대신 ' '를 사용해서 공백을 제거한다. '\s'가 맥에서 한글 파일명을 sanitize할 때 이상한 현상을 일으키기 때문이다.
+ * 2. apply_filter를 제거했다.
+ * @param  string $filename
+ * @return string
+ */
+function nlf_sanitize_file_name( $filename ){
+	$filename_raw = $filename;
+	$special_chars = array("?", "[", "]", "/", "\\", "=", "<", ">", ":", ";", ",", "'", "\"", "&", "$", "#", "*", "(", ")", "|", "~", "`", "!", "{", "}", chr(0));
+	$special_chars = apply_filters('sanitize_file_name_chars', $special_chars, $filename_raw);
+	$filename = str_replace($special_chars, '', $filename);
+	$filename = preg_replace('/[ -]+/', '-', $filename);
+	$filename = trim($filename, '.-_');
+	
+	// Split the filename into a base and extension[s]
+	$parts = explode('.', $filename);
+
+	// Return if only one extension
+	if ( count($parts) <= 2 ){
+		return $filename;
+	}
+
+	// Process multiple extensions
+	$filename = array_shift($parts);
+	$extension = array_pop($parts);
+	$mimes = get_allowed_mime_types();
+
+	// Loop over any intermediate extensions. Munge them with a trailing underscore if they are a 2 - 5 character
+	// long alpha string not in the extension whitelist.
+	foreach ( (array) $parts as $part) {
+		$filename .= '.' . $part;
+
+		if ( preg_match("/^[a-zA-Z]{2,5}\d?$/", $part) ) {
+			$allowed = false;
+			foreach ( $mimes as $ext_preg => $mime_match ) {
+				$ext_preg = '!^(' . $ext_preg . ')$!i';
+				if ( preg_match( $ext_preg, $part ) ) {
+					$allowed = true;
+					break;
+				}
+			}
+			if ( !$allowed )
+				$filename .= '_';
+		}
+	}
+	$filename .= '.' . $extension;
+
+	return $filename;
+}
 
 //end of non-latin.php
